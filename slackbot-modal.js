bccdc1113ab5c91b61b1bc1e52254fd0a6256fe7
Matthew Leonard <mattleonardco@gmail.com>
2019-10-14 17:09:13 -0600

    Put text in desc if messagee is too long

--- slackbot-modal.js
+++ slackbot-modal.js
@@ -1,43 +1,48 @@
-module.exports = message => [
-  {
-    name: "uf-title",
-    type: "text",
-    label: "Title",
-    value: message,
-    element: {
-      type: "plain_text_input",
-      placeholder: {
-        type: "plain_text",
-        text: "Short, descriptive title"
+module.exports = message => {
+  const messageLong = message.length > 150;
+
+  return [
+    {
+      name: "uf-title",
+      type: "text",
+      label: "Title",
+      value: !messageLong ? message : "",
+      element: {
+        type: "plain_text_input",
+        placeholder: {
+          type: "plain_text",
+          text: "Short, descriptive title"
+        }
       }
+    },
+    {
+      name: "uf-desc",
+      type: "textarea",
+      label: "Description",
+      value: messageLong ? message : "",
+      element: {
+        type: "plain_text_input",
+        multiline: true,
+        placeholder: {
+          type: "plain_text",
+          text: "When ____, I want to ____, so I can ____"
+        }
+      }
+    },
+    {
+      name: "uf-feed",
+      type: "select",
+      label: "Feed",
+      options: [
+        {
+          label: "🐛Bugs",
+          value: "bug"
+        },
+        {
+          label: "✨Feature Requests",
+          value: "feature"
+        }
+      ]
     }
-  },
-  {
-    name: "uf-desc",
-    type: "textarea",
-    label: "Description",
-    element: {
-      type: "plain_text_input",
-      multiline: true,
-      placeholder: {
-        type: "plain_text",
-        text: "When ____, I want to ____, so I can ____"
-      }
-    }
-  },
-  {
-    name: "uf-feed",
-    type: "select",
-    label: "Feed",
-    options: [
-      {
-        label: "🐛Bugs",
-        value: "bug"
-      },
-      {
-        label: "✨Feature Requests",
-        value: "feature"
-      }
-    ]
-  }
-];
+  ];
+};
